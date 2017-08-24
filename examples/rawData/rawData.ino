ce60dd9fef4c6392a578b84e2b7a9a2b763e0cd2
Bill2462 <burza7@vp.pl>
2017-08-24 13:37:25 +0200

    Update rawData.ino

--- examples/rawData/rawData.ino
+++ examples/rawData/rawData.ino
@@ -17,22 +17,28 @@ void setup() {
 
 void loop() {
   mpu.read_acc();// read data from accelerometer
-  int ax=mpu.get_ax();// read acceleration in X axis
-  int ay=mpu.get_ay();// read acceleration in Y axis
-  int az=mpu.get_az();// read acceleration in Z axis
-  
   mpu.read_gyro();// get data from gyroscope
-  int gx=mpu.get_gx();// read rotation in X axis
-  int gy=mpu.get_gy();// read rotation in Y axis
-  int gz=mpu.get_gz();// read rotation in Z axis
-  
   mpu.read_mag();// get data from the magnetometer 
-  int mx=mpu.get_mx();// return magnetic field data from X axis 
-  int my=mpu.get_my();// return magnetic field data from Y axis 
-  int mz=mpu.get_mz();// return magnetic field data from Z axis 
+
   // print all data in serial monitor
-  Serial.print("AX: "); Serial.print(ax); Serial.print(" AY: "); Serial.print(ay); Serial.print(" AZ: "); Serial.print(az);// accelerometer data 
-  Serial.print("  GX: "); Serial.print(gx); Serial.print(" GY: "); Serial.print(gy); Serial.print(" GZ: "); Serial.print(gz);// gyroscope data
-  Serial.print("  MX: "); Serial.print(mx); Serial.print(" MY: "); Serial.print(my); Serial.print(" MZ: "); Serial.println(mz);// gyroscope data
+  Serial.print("AX: "); 
+  Serial.print(mpu.ax); 
+  Serial.print(" AY: ");
+  Serial.print(mpu.ay); 
+  Serial.print(" AZ: "); 
+  Serial.print(mpu.az);// accelerometer data 
+  Serial.print("  GX: "); 
+  Serial.print(mpu.gx); 
+  Serial.print(" GY: "); 
+  Serial.print(mpu.gy); 
+  Serial.print(" GZ: "); 
+  Serial.print(mpu.gz);// gyroscope data
+  Serial.print("  MX: "); 
+  Serial.print(mpu.mx); 
+  Serial.print(" MY: "); 
+  Serial.print(mpu.my); 
+  Serial.print(" MZ: "); 
+  Serial.println(mpu.mz);// gyroscope data
+  delay(100);
   delay(100);
 }
