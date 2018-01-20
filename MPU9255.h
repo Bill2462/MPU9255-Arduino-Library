4deb1fc961bb33f7adaaff69354f6bd1d5b90f68
Bill2462 <burza7@vp.pl>
2018-01-20 19:18:08 +0100

    Update MPU9255.h

--- MPU9255.h
+++ MPU9255.h
@@ -1,31 +1,25 @@
 /*
-Copyright (C) Bill2462 from https://github.com/Bill2462 
+Copyright (C) Bill2462 from https://github.com/Bill2462
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
  version 3 as published by the Free Software Foundation.
-
 */
 
 #ifndef MPU9255_H
 #define MPU9255_H
- 
+
 #include <Arduino.h>
-#include <Wire.h> 
+#include <Wire.h>
 
-//############################################# Definitions ############################################# 
+//sensor adresses
+#define MAG_adress 0x0C//magnetometer
+#define MPU_adress 0x68//gyroscope and accelerometer
 
-// definitions of sensors adresses 
-#define MAG_adress 0x0C// magnetometer 
-#define MPU_adress 0x68// main chip adress
-
-// registers adresses 
-
-//control registers 
+//control registers
 #define USER_CTRL  0x6A
 #define PWR_MGMT_2 0x6B
 #define INT_PIN_CFG 0x37
 #define ST1 0x02
-//#define ST2 0x09
 #define CNTL 0x0A
 #define ACCEL_CONFIG 0x1C
 #define GYRO_CONFIG 0x1B
@@ -36,49 +30,40 @@ Copyright (C) Bill2462 from https://github.com/Bill2462
 #define INT_PIN_CFG 0x37
 #define INT_ENABLE 0x38
 
-// data registers 
+/// data registers
+#define MAG_XOUT_L 0x03//magnetometer
+#define GYRO_XOUT_H 0x43//gyro
+#define ACCEL_XOUT_H 0x3B//accelerometer
 
-// magnetometer 
-#define MAG_XOUT_L 0x03
-
-// gyroscope
-#define GYRO_XOUT_H 0x43
-
-//accelerometer 
-#define ACCEL_XOUT_H 0x3B
-
-//############################################# MPU9255 class ################################################
-
-class MPU9255 
+class MPU9255
 {
-
 public:
- // ---- Variabled ----
-int16_t ax=0;
-int16_t ay=0;
-int16_t az=0;
 
-int16_t gx=0;
-int16_t gy=0;
-int16_t gz=0;
+//acceleration data
+int16_t ax=0;//X axis
+int16_t ay=0;//Y axis
+int16_t az=0;//Z axis
 
-int16_t mx=0;
-int16_t my=0;
-int16_t mz=0;
- 
-// ---- General functions ----- 
-void init();
-void set_acc_scale(unsigned char value);
-void set_gyro_scale(unsigned char value);
- 
-// ---- Data read functions ----- 
-void read_acc();
-void read_gyro();
-void read_mag();
- 
+//gyroscope data
+int16_t gx=0;//X axis
+int16_t gy=0;//Y axis
+int16_t gz=0;//Z axis
+
+//magnetometer data
+int16_t mx=0;//X axis
+int16_t my=0;//Y axis
+int16_t mz=0;//Z axis
+
+void init();//Initialize magnetometer and IMU
+void set_acc_scale(unsigned char value);//Set accelerometer scale
+void set_gyro_scale(unsigned char value);//Set gyroscope scale
+void read_acc();//read data from accelerometer
+void read_gyro();//read data from gyroscope
+void read_mag();//read data from magnetometer
+
 private:
-uint8_t read(uint8_t address, uint8_t subAddress);
-void write(uint8_t address, uint8_t subAddress, uint8_t data);
+uint8_t read(uint8_t address, uint8_t subAddress);//read one byte from register
+void write(uint8_t address, uint8_t subAddress, uint8_t data);//write one byte of data to the register
 
 };
 
